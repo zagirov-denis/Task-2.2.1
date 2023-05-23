84755d50bb754748cf5737cba9950d9a43744358
Denzmc <degtyar87@mail.ru>
2023-05-23 17:42:58 +0500

    hiber_commit_1

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,11 +15,13 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      Car car = new Car("BMW", 525);
+      Car car2 = new Car("AUDI", 80);
 
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", car2));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +29,15 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " + user.getCar());
          System.out.println();
       }
+
+      User user = userService.getUserByCar("BMW", 525);
+      User user2 = userService.getUserByCar("AUDI", 80);
+      System.out.println(user.getFirstName());
+      System.out.println(user2.getFirstName());
+
 
       context.close();
    }
